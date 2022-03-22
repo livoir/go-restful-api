ac6617d2e52caf007b10c7e30c3aaec7f6fb46ae
iqbalalfikri <iqbalalfikri12@gmail.com>
2022-03-22 13:55:40 +0700

    Add Integration Test

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"belajar-golang-restful-api/app"
 	"belajar-golang-restful-api/controller"
-	"belajar-golang-restful-api/exception"
 	"belajar-golang-restful-api/helper"
 	"belajar-golang-restful-api/middleware"
 	"belajar-golang-restful-api/repository"
 	"belajar-golang-restful-api/service"
 	"github.com/go-playground/validator/v10"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
@@ -20,15 +18,7 @@ func main() {
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
 
-	router := httprouter.New()
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-
-	router.PanicHandler = exception.ErrorHandler
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
